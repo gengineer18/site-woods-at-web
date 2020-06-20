3c60c9fcac3754b789a3faef29d53fc3efe9368b
妹尾弦 <gengineer.web@gmail.com>
2020-06-20 22:55:27 +0900

    ディレクトリ構造の変更

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
-export { default as SEO } from './seo';
-export { default as WrapPageElement } from './wrap-page-element';
-export { default as WrapRootElement } from './wrap-root-element';
-export { default as AnyImage } from './any-image';
+export { default as SEO } from './utils/seo';
+export { default as WrapPageElement } from './utils/wrap-page-element';
+export { default as WrapRootElement } from './utils/wrap-root-element';
+export { default as AnyImage } from './utils/any-image';
