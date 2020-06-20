d17a711569c4121f70f4c3d6e51474a3bb83d1a3
妹尾弦 <gengineer.web@gmail.com>
2020-06-20 22:53:21 +0900

    不要ファイルの削除

--- src/components/wrap-root-element.tsx
+++ src/components/wrap-root-element.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from 'src/layouts';
-import { SwitchContainer } from 'src/store';
 
 import styled from '@emotion/styled';
 
@@ -8,11 +7,7 @@ type ContainerProps = { element: React.ReactNode };
 type Props = ContainerProps;
 
 // you can use this component for some providers for redux, context API, etc...
-const Component: React.FCX<Props> = ({ element }) => (
-  <SwitchContainer.Provider>
-    <Layout>{element}</Layout>
-  </SwitchContainer.Provider>
-);
+const Component: React.FCX<Props> = ({ element }) => <Layout>{element}</Layout>;
 
 const StyledComponent = styled(Component)``;
 
