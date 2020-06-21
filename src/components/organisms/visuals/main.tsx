f0066a319af4a3f1c40ef63033ec647c70c9fd3f
妹尾弦 <gengineer.web@gmail.com>
2020-06-21 20:18:10 +0900

    main部分

--- src/components/organisms/visuals/main.tsx
+++ src/components/organisms/visuals/main.tsx
@@ -3,12 +3,18 @@ import { css } from '@emotion/core';
 import { visuals } from '../../../styles/index';
 
 const main = css({
-  borderTop: `10px solid blue`,
-  borderBottom: `10px solid red`,
+  background: `linear-gradient(blue, white 25% 75%, red 100%)`,
+  display: `flex`,
+  justifyContent: `center`,
+  alignItems: `center`,
+});
+
+const heading = css({
+  fontSize: `4.8rem`,
 });
 
 export const Main: React.FCX = () => (
   <div css={[visuals, main]}>
-    <h1>Woods At Web</h1>
+    <h1 css={heading}>Woods At Web</h1>
   </div>
 );
